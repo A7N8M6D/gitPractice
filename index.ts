9b5826c1113122af7a9bff5d1789a05b23f9ef73
ANMD <nosha3173@gmail.com>
2024-11-05 20:45:53 +0500

    Typescript in nodejs added

---
+++ index.ts
@@ -0,0 +1,9 @@
+let namee:string="Ali"
+console.log(namee)
+function add( a:number , b:number):number{
+    console.log(`First Number=${a} | Second Number=${b}`)
+
+return a+b
+}
+
+console.log(add(2,3))
